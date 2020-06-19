756af9de9e6978abca4260ed6b39653482662986
Varun Teja Nanduri <vamsi150895@gmail.com>
2020-06-19 18:55:21 -0500

    Work in progress

--- arrivalOfGeneral.py
+++ arrivalOfGeneral.py
@@ -1,15 +1,18 @@
 # Python program for the Arrival of general problem from codeForce
 
 
-print('Enter the heights of the soldiers: ')
+print('How many soldiers in the group? ')
 sol = int(input())
 
-print('Enter the heights for the,'sol,  ' soldiers: ')
+print('Enter the heights for the', sol,  ' soldiers: ')
 
 heights = []
-
 heights = input().split()
 
-heights = int(heights)
-
-print(heights)
+if len(heights) < sol:
+    print('Re- Enter the heights and number of soldiers')
+else:    
+    for i in range(0, len(heights)): 
+        heights[i] = int(heights[i]) 
+    print("The heights are: ", heights)
+    
