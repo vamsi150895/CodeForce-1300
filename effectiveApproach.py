eb13732b9fb26ed00ce4f706e39e5045c85f64ce
Varun Teja Nanduri <vamsi150895@gmail.com>
2020-06-22 15:51:58 -0500

    working

--- effectiveApproach.py
+++ effectiveApproach.py
@@ -1,2 +1,27 @@
 # Python program for the Effective Approach problem from codeForce
 
+
+# Input part 
+vasya_approach = 0
+petya_approach = 0
+print('Enter the values as per the problem requirement')
+numberOfele = int(input())
+elements = list(map(int,input().split()))
+numberOfQue = int(input())
+searchQue = list(map(int,input().split()))
+
+#Vasya's approach
+
+
+
+
+#Petya's approach
+
+
+
+
+
+
+
+print(vasya_approach, petya_approach)
+
