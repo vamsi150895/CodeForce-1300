fd440e8ae0b42dc4d2fdc61ab475d36da376377c
Varun Teja Nanduri <vamsi150895@gmail.com>
2020-06-28 16:59:20 -0500

    Petr and Book

--- PetrBook.py
+++ PetrBook.py
@@ -3,10 +3,11 @@
 print('Enter number of pages in the book')
 noOfPages = int(input())
 print('Enter the number of pages read on each day of the week')
-pagesPerWeek = list(map(int, input().split()))
+pagesPerDay = list(map(int, input().split()))
 sum1 = 0
-for weekDay in pagesPerWeek:
-    sum1 = sum1 + weekDay
-    if sum1 >= noOfPages:
-        print(pagesPerWeek.index(weekDay) + 1)
-        exit()
+while(sum1 < noOfPages):
+    for weekDay in pagesPerDay:
+        sum1 = sum1 + weekDay
+        if sum1 >= noOfPages:
+            print(pagesPerDay.index(weekDay) + 1)
+            exit()
