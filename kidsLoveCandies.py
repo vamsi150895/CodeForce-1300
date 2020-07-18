5e3501c76be061ee45825f7781cb68c3ddc80dcb
Varun Teja Nanduri <vamsi150895@gmail.com>
2020-07-17 21:14:24 -0500

    Kids Love Candies

--- kidsLoveCandies.py
+++ kidsLoveCandies.py
@@ -1,2 +1,33 @@
 # Python program for the Kids love candies problem from codeForce
 
+#t = int(input('Enter number of test cases: '))
+t = int(input())
+minChild = []
+sol = []
+sum1 = 0 
+for i in range(t): 
+    #print(i+1,' test case:')
+    #n,k = map(int, input('Enter number of Candies and minimum for each kid ').split())
+    n,k = map(int, input('').split())
+    #N = list(map(int, input('Enter number of Candies of each type').split()))
+    N = list(map(int, input().split()))
+    if(len(N) != n):
+        print('Error, Enter ', n ,' elements')
+        exit()
+    ###########################################    
+    i = -1   
+    sum1 = 0     
+    for element in N:
+        i = i+1
+        if element < k:
+            continue
+        elif True:
+            sum1 = sum1 + int((element) / k)
+        else:
+            print(0)       
+    sol.append(sum1)
+    
+    
+for ele in sol:            
+    print(ele)
+    
